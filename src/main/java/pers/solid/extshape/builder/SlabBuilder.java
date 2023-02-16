1a3526b07f9808260c2599f2a37c3e01ee5849c1
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2023-02-16 17:49:57 +0800

    优化与 registry 有关的内容，以便于多平台（适应 Forge） 优化代码格式，修复代码中的一些问题 更新至 2.0.0-beta.1

--- src/main/java/pers/solid/extshape/builder/SlabBuilder.java
+++ src/main/java/pers/solid/extshape/builder/SlabBuilder.java
@@ -2,20 +2,24 @@ package pers.solid.extshape.builder;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SlabBlock;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.tag.BlockTags;
+import pers.solid.extshape.block.BlockExtension;
 import pers.solid.extshape.block.ExtShapeSlabBlock;
-import pers.solid.extshape.tag.ExtShapeTags;
 
 public class SlabBuilder extends AbstractBlockBuilder<SlabBlock> {
   public SlabBuilder(Block baseBlock) {
     super(baseBlock, builder -> new ExtShapeSlabBlock(baseBlock, builder.blockSettings));
-    this.defaultTagToAdd = ExtShapeTags.SLABS;
     this.shape = BlockShape.SLAB;
-    itemSettings.group(ItemGroup.BUILDING_BLOCKS);
+    primaryTagToAddTo = BlockTags.SLABS;
   }
 
   @Override
   protected String getSuffix() {
     return "_slab";
+  }
+
+  @Override
+  public AbstractBlockBuilder<SlabBlock> withExtension(BlockExtension blockExtension) {
+    return setInstanceSupplier(builder -> new ExtShapeSlabBlock.WithExtension(baseBlock, builder.blockSettings, blockExtension));
   }
 }
