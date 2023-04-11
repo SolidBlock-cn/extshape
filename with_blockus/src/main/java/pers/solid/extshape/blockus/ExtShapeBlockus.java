7a53eca80e9d42a945ad3a4527b0383d884219b7
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2023-04-11 21:50:59 +0800

    修复 Blockus 中的一些问题，包括白色橡木的去皮

--- with_blockus/src/main/java/pers/solid/extshape/blockus/ExtShapeBlockus.java
+++ with_blockus/src/main/java/pers/solid/extshape/blockus/ExtShapeBlockus.java
@@ -1,9 +1,13 @@
 package pers.solid.extshape.blockus;
 
+import com.brand.blockus.content.BlockusBlocks;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pers.solid.extshape.ExtShape;
+import pers.solid.extshape.builder.BlockShape;
+import pers.solid.extshape.util.BlockBiMaps;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -39,6 +43,22 @@ public class ExtShapeBlockus implements ModInitializer {
       LOGGER.info("Blockus mod loaded. Extended Block Shapes mod is trying to apply it.");
       ExtShapeBlockusBlocks.init();
       ExtShapeBlockusRRP.registerRRP();
+      registerStrippableBlocks();
+    }
+  }
+
+  private static void registerStrippableBlocks() {
+    for (BlockShape shape : BlockShape.values()) {
+      var block1 = BlockBiMaps.getBlockOf(shape, BlockusBlocks.WHITE_OAK_LOG);
+      var block2 = BlockBiMaps.getBlockOf(shape, BlockusBlocks.STRIPPED_WHITE_OAK_LOG);
+      if (block1 != null && block2 != null) {
+        ExtShape.EXTENDED_STRIPPABLE_BLOCKS.put(block1, block2);
+      }
+      var block3 = BlockBiMaps.getBlockOf(shape, BlockusBlocks.WHITE_OAK_WOOD);
+      var block4 = BlockBiMaps.getBlockOf(shape, BlockusBlocks.STRIPPED_WHITE_OAK_WOOD);
+      if (block3 != null && block4 != null) {
+        ExtShape.EXTENDED_STRIPPABLE_BLOCKS.put(block3, block4);
+      }
     }
   }
 }
