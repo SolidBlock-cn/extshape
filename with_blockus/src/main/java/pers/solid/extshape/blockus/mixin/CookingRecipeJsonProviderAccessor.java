7293a4968d3c1d63499c7381af7e89b015d29304
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2023-04-15 08:13:22 +0800

    准备更新至 2.0.0，更新文档

--- with_blockus/src/main/java/pers/solid/extshape/blockus/mixin/CookingRecipeJsonProviderAccessor.java
+++ with_blockus/src/main/java/pers/solid/extshape/blockus/mixin/CookingRecipeJsonProviderAccessor.java
@@ -1,10 +1,11 @@
 package pers.solid.extshape.blockus.mixin;
 
 import net.minecraft.advancement.Advancement;
+import net.minecraft.data.server.recipe.CookingRecipeJsonBuilder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(targets = "net.minecraft.data.server.recipe.CookingRecipeJsonBuilder$CookingRecipeJsonProvider")
+@Mixin(CookingRecipeJsonBuilder.CookingRecipeJsonProvider.class)
 public
 interface CookingRecipeJsonProviderAccessor {
   @Accessor
