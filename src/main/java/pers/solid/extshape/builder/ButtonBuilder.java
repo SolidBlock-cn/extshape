f01f6ae2ed07ba7450f1d1d1d5b74c874f4b3fa6
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2023-10-10 15:31:34 +0800

    - 修改了方块的活塞行为以符合基础方块。其中，不能被推动的方块，其压力板和按钮也不能被活塞推动，其他方块的压力板和按钮会被活塞直接破坏。 - 修改了方块的音符盒乐器以符合原版。各方块（除按钮外）的音符盒乐器与其基础方块相同，但按钮的音符盒乐器一律为竖琴（即默认乐器）。

--- src/main/java/pers/solid/extshape/builder/ButtonBuilder.java
+++ src/main/java/pers/solid/extshape/builder/ButtonBuilder.java
@@ -1,8 +1,10 @@
 package pers.solid.extshape.builder;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.ButtonBlock;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.sound.BlockSoundGroup;
 import org.jetbrains.annotations.NotNull;
@@ -12,7 +14,11 @@ import pers.solid.extshape.util.ButtonSettings;
 public class ButtonBuilder extends AbstractBlockBuilder<ButtonBlock> {
 
   public ButtonBuilder(@NotNull ButtonSettings buttonSettings, Block baseBlock) {
-    super(baseBlock, FabricBlockSettings.copyOf(baseBlock).noCollision().strength(baseBlock.getHardness() / 4f), builder -> new ExtShapeButtonBlock(baseBlock, buttonSettings, builder.blockSettings));
+    super(baseBlock, AbstractBlock.Settings.copy(baseBlock)
+        .noCollision()
+        .strength(baseBlock.getHardness() / 4f)
+        .pistonBehavior(baseBlock.getDefaultState().getPistonBehavior() == PistonBehavior.BLOCK ? PistonBehavior.BLOCK : PistonBehavior.DESTROY)
+        .instrument(Instrument.HARP), builder -> new ExtShapeButtonBlock(baseBlock, buttonSettings, builder.blockSettings));
     this.shape = BlockShape.BUTTON;
     final BlockSoundGroup soundGroup = baseBlock.getSoundGroup(baseBlock.getDefaultState());
     primaryTagToAddTo = soundGroup == BlockSoundGroup.WOOD || soundGroup == BlockSoundGroup.NETHER_WOOD || soundGroup == BlockSoundGroup.BAMBOO_WOOD || soundGroup == BlockSoundGroup.CHERRY_WOOD ? BlockTags.WOODEN_BUTTONS : BlockTags.BUTTONS;
--- src/main/java/pers/solid/extshape/builder/PressurePlateBuilder.java
+++ src/main/java/pers/solid/extshape/builder/PressurePlateBuilder.java
@@ -1,9 +1,10 @@
 package pers.solid.extshape.builder;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSetType;
 import net.minecraft.block.PressurePlateBlock;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.sound.BlockSoundGroup;
 import org.jetbrains.annotations.NotNull;
@@ -16,7 +17,10 @@ public class PressurePlateBuilder extends AbstractBlockBuilder<PressurePlateBloc
   protected final BlockSetType blockSetType;
 
   public PressurePlateBuilder(@NotNull PressurePlateBlock.ActivationRule type, Block baseBlock, @NotNull BlockSetType blockSetType) {
-    super(baseBlock, FabricBlockSettings.copyOf(baseBlock).noCollision().strength(baseBlock.getHardness() / 4f), builder -> new ExtShapePressurePlateBlock(builder.baseBlock, ((PressurePlateBuilder) builder).type, builder.blockSettings, ((PressurePlateBuilder) builder).blockSetType));
+    super(baseBlock, AbstractBlock.Settings.copy(baseBlock)
+        .noCollision()
+        .strength(baseBlock.getHardness() / 4f)
+        .pistonBehavior(baseBlock.getDefaultState().getPistonBehavior() == PistonBehavior.BLOCK ? PistonBehavior.BLOCK : PistonBehavior.DESTROY), builder -> new ExtShapePressurePlateBlock(builder.baseBlock, ((PressurePlateBuilder) builder).type, builder.blockSettings, ((PressurePlateBuilder) builder).blockSetType));
     this.shape = BlockShape.PRESSURE_PLATE;
     this.type = type;
     this.blockSetType = blockSetType;
