0a29537b1c99cbfc62ed252539ad29d3de7b68cf
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2024-01-31 19:18:28 +0800

    再次调整了部分压力板和按钮方块的激活时长

--- src/main/java/pers/solid/extshape/util/ActivationSettings.java
+++ src/main/java/pers/solid/extshape/util/ActivationSettings.java
@@ -22,11 +22,11 @@ public record ActivationSettings(BlockSetType blockSetType, int buttonTime, int 
   }
 
   public static ActivationSettings soft(BlockSetType blockSetType) {
-    return new ActivationSettings(blockSetType, 60, 60, true, PressurePlateBlock.ActivationRule.EVERYTHING);
+    return new ActivationSettings(blockSetType, 50, 40, true, PressurePlateBlock.ActivationRule.EVERYTHING);
   }
 
   public static ActivationSettings softerStone(BlockSetType blockSetType) {
-    return new ActivationSettings(blockSetType, 40, 30, false, PressurePlateBlock.ActivationRule.MOBS);
+    return new ActivationSettings(blockSetType, 25, 20, false, PressurePlateBlock.ActivationRule.MOBS);
   }
 
   public static ActivationSettings wood(BlockSetType blockSetType) {
@@ -43,18 +43,22 @@ public record ActivationSettings(BlockSetType blockSetType, int buttonTime, int 
     return new ActivationSettings(ExtShapeBlockTypes.COPPER_BLOCK_SET_TYPE, activationRate, activationRate, false, PressurePlateBlock.ActivationRule.MOBS);
   }));
   public static final ActivationSettings HARD = hard(ExtShapeBlockTypes.HARD_BLOCK_SET_TYPE);
-  public static final ActivationSettings WOOL = new ActivationSettings(ExtShapeBlockTypes.WOOL_BLOCK_SET_TYPE, 50, 50, true, PressurePlateBlock.ActivationRule.EVERYTHING);
+  public static final ActivationSettings WOOL = new ActivationSettings(ExtShapeBlockTypes.WOOL_BLOCK_SET_TYPE, 50, 40, true, PressurePlateBlock.ActivationRule.EVERYTHING);
+  public static final ActivationSettings SNOW = new ActivationSettings(ExtShapeBlockTypes.SNOW_BLOCK_SET_TYPE, 55, 55, true, PressurePlateBlock.ActivationRule.EVERYTHING);
+  public static final ActivationSettings MOSS = new ActivationSettings(ExtShapeBlockTypes.MOSS_BLOCK_SET_TYPE, 60, 60, true, PressurePlateBlock.ActivationRule.EVERYTHING);
   public static final ActivationSettings BAMBOO = wood(BlockSetType.BAMBOO);
-  public static final ActivationSettings GRAVEL = soft(ExtShapeBlockTypes.GRAVEL_BLOCK_SET_TYPE);
+  public static final ActivationSettings GRAVEL = new ActivationSettings(ExtShapeBlockTypes.GRAVEL_BLOCK_SET_TYPE, 45, 45, true, PressurePlateBlock.ActivationRule.EVERYTHING);
   public static final ActivationSettings ORE_BLOCK = new ActivationSettings(BlockSetType.STONE, 15, 10, false, PressurePlateBlock.ActivationRule.MOBS);
   public static final ActivationSettings GOLD = new ActivationSettings(BlockSetType.GOLD, 15, 10, false, PressurePlateBlock.ActivationRule.MOBS);
   public static final ActivationSettings IRON = new ActivationSettings(BlockSetType.IRON, 15, 10, false, PressurePlateBlock.ActivationRule.MOBS);
-  public static final ActivationSettings CROPS = new ActivationSettings(BlockSetType.OAK, 50, 40, false, PressurePlateBlock.ActivationRule.MOBS);
-  public static final ActivationSettings QUARTZ = new ActivationSettings(ExtShapeBlockTypes.QUARTZ_BLOCK_SET_TYPE, 25, 25, false, PressurePlateBlock.ActivationRule.MOBS);
-  public static final ActivationSettings BASALT = stone(ExtShapeBlockTypes.BASALT_BLOCK_SET_TYPE);
+  public static final ActivationSettings CROPS = new ActivationSettings(BlockSetType.OAK, 35, 25, false, PressurePlateBlock.ActivationRule.MOBS);
+  public static final ActivationSettings QUARTZ = new ActivationSettings(ExtShapeBlockTypes.QUARTZ_BLOCK_SET_TYPE, 25, 20, false, PressurePlateBlock.ActivationRule.MOBS);
+  public static final ActivationSettings BASALT = new ActivationSettings(ExtShapeBlockTypes.BASALT_BLOCK_SET_TYPE, 15, 15, false, PressurePlateBlock.ActivationRule.MOBS);
   public static final ActivationSettings DRIPSTONE = stone(ExtShapeBlockTypes.DRIPSTONE_BLOCK_SET_TYPE);
   public static final ActivationSettings TUFF = softerStone(ExtShapeBlockTypes.TUFF_BLOCK_SET_TYPE);
-  public static final ActivationSettings DEEPSLATE = stone(ExtShapeBlockTypes.DEEPSLATE_BLOCK_SET_TYPE);
-  public static final ActivationSettings DEEPSLATE_BRICKS = stone(ExtShapeBlockTypes.DEEPSLATE_BRICKS_BLOCK_SET_TYPE);
+  public static final ActivationSettings DEEPSLATE = new ActivationSettings(ExtShapeBlockTypes.DEEPSLATE_BLOCK_SET_TYPE, 10, 10, false, PressurePlateBlock.ActivationRule.MOBS);
+  public static final ActivationSettings DEEPSLATE_BRICKS = new ActivationSettings(ExtShapeBlockTypes.DEEPSLATE_BRICKS_BLOCK_SET_TYPE, 10, 10, false, PressurePlateBlock.ActivationRule.MOBS);
   public static final ActivationSettings NETHERRACK = softerStone(ExtShapeBlockTypes.NETHERRACK_BLOCK_SET_TYPE);
+  public static final ActivationSettings GLOWSTONE = new ActivationSettings(ExtShapeBlockTypes.GLOWSTONE_BLOCK_SET_TYPE, 30, 30, true, PressurePlateBlock.ActivationRule.EVERYTHING);
+  public static final ActivationSettings WART = new ActivationSettings(ExtShapeBlockTypes.WART_BLOCK_SET_TYPE, 45, 45, true, PressurePlateBlock.ActivationRule.EVERYTHING);
 }
