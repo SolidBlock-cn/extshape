d8543e7caa17ddf30f20fa5d0f8e6155394aa31c
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2024-01-31 19:18:28 +0800

    再次调整了部分压力板和按钮方块的激活时长

--- src/main/java/pers/solid/extshape/util/FenceSettings.java
+++ src/main/java/pers/solid/extshape/util/FenceSettings.java
@@ -4,6 +4,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
+import pers.solid.extshape.ExtShape;
 
 /**
  * 存储用于栅栏和栅栏门的一些设置，在构建方块时会使用到。
@@ -15,6 +17,10 @@ public record FenceSettings(Item secondIngredient, SoundEvent closeSound, SoundE
     this(secondIngredient, SoundEvents.BLOCK_FENCE_GATE_CLOSE, SoundEvents.BLOCK_FENCE_GATE_OPEN);
   }
 
+  public FenceSettings(Item secondIngredient, String namespace, String constSoundName) {
+    this(secondIngredient, new SoundEvent(new Identifier(namespace, "block." + constSoundName + "_fence_gate.close")), new SoundEvent(new Identifier(namespace, "block." + constSoundName + "_fence_gate.open")));
+  }
+
   public FenceSettings(Item secondIngredient, SoundEvent constSound) {
     this(secondIngredient, constSound, constSound);
   }
@@ -22,12 +28,12 @@ public record FenceSettings(Item secondIngredient, SoundEvent closeSound, SoundE
   public static final FenceSettings DEFAULT = new FenceSettings(Items.STICK, SoundEvents.BLOCK_FENCE_GATE_CLOSE, SoundEvents.BLOCK_FENCE_GATE_OPEN);
   public static final FenceSettings WOOD = DEFAULT;
   public static final FenceSettings NETHER_WOOD = WOOD;
-  public static final FenceSettings AMETHYST = new FenceSettings(Items.AMETHYST_SHARD, SoundEvents.BLOCK_AMETHYST_BLOCK_HIT);
+  public static final FenceSettings AMETHYST = new FenceSettings(Items.AMETHYST_SHARD, ExtShape.MOD_ID, "amethyst");
   public static final FenceSettings BAMBOO_BLOCK = new FenceSettings(Items.BAMBOO);
   public static final FenceSettings STONE = new FenceSettings(Items.FLINT);
   public static final FenceSettings DIRT = DEFAULT;
-  public static final FenceSettings WOOL = new FenceSettings(Items.STRING, SoundEvents.BLOCK_WOOL_HIT);
-  public static final FenceSettings SNOW = new FenceSettings(Items.SNOW, SoundEvents.BLOCK_SNOW_HIT);
+  public static final FenceSettings WOOL = new FenceSettings(Items.STRING, ExtShape.MOD_ID, "wool");
+  public static final FenceSettings SNOW = new FenceSettings(Items.SNOW, ExtShape.MOD_ID, "snow");
   public static final FenceSettings QUARTZ = new FenceSettings(Items.QUARTZ);
   public static final FenceSettings BASALT = STONE;
   public static final FenceSettings DRIPSTONE = new FenceSettings(Items.POINTED_DRIPSTONE);
@@ -35,5 +41,5 @@ public record FenceSettings(Item secondIngredient, SoundEvent closeSound, SoundE
   public static final FenceSettings DEEPSLATE = STONE;
   public static final FenceSettings DEEPSLATE_BRICKS = STONE;
   public static final FenceSettings NETHERRACK = new FenceSettings(Items.NETHER_BRICK);
-  public static final FenceSettings WART = new FenceSettings(Items.NETHER_WART, SoundEvents.BLOCK_WART_BLOCK_HIT);
+  public static final FenceSettings WART = new FenceSettings(Items.NETHER_WART, ExtShape.MOD_ID, "wart");
 }
