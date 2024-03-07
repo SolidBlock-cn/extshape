9be6dfc1ecd830986c19a48b57a615b810b25d06
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2024-03-07 22:56:03 +0800

    update to 2.1.2 - Optimized the logic of item display in creative inventory and items are displayed faster. - Fixed a crash error related to creative inventory along with Sinytra Connector. - Improved some language files.

--- src/main/java/pers/solid/extshape/mixin/ItemGroupEntriesImplMixin.java
+++ src/main/java/pers/solid/extshape/mixin/ItemGroupEntriesImplMixin.java
@@ -1,10 +1,11 @@
 package pers.solid.extshape.mixin;
 
+import com.google.common.collect.Multimap;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
-import net.minecraft.registry.RegistryKey;
+import net.minecraft.resource.featuretoggle.FeatureSet;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,7 +16,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import pers.solid.extshape.VanillaItemGroup;
 import pers.solid.extshape.config.ExtShapeConfig;
 
-import java.util.Optional;
+import java.util.Collection;
+import java.util.Set;
 
 @Mixin(targets = "net.minecraft.item.ItemGroup$EntriesImpl")
 public abstract class ItemGroupEntriesImplMixin {
@@ -24,38 +26,54 @@ public abstract class ItemGroupEntriesImplMixin {
   private ItemGroup group;
 
   @Shadow
-  public abstract void add(ItemStack stack, ItemGroup.StackVisibility visibility);
+  @Final
+  private FeatureSet enabledFeatures;
+
+  @Shadow
+  @Final
+  public Collection<ItemStack> parentTabStacks;
+
+  @Shadow
+  @Final
+  public Set<ItemStack> searchTabStacks;
 
   @Unique
-  private boolean allowTransitive = true;
+  public void addSwiftly(ItemStack stack, ItemGroup.StackVisibility visibility) {
+    if (stack.getItem().isEnabled(enabledFeatures)) {
+      switch (visibility) {
+        case PARENT_AND_SEARCH_TABS:
+          this.parentTabStacks.add(stack);
+          this.searchTabStacks.add(stack);
+          break;
+        case PARENT_TAB_ONLY:
+          this.parentTabStacks.add(stack);
+          break;
+        case SEARCH_TAB_ONLY:
+          this.searchTabStacks.add(stack);
+      }
+    }
+  }
+
+  @Unique
+  private final Multimap<Item, Item> prependingRule = ExtShapeConfig.CURRENT_CONFIG.addToVanillaGroups ? Registries.ITEM_GROUP.getKey(this.group).map(VanillaItemGroup::getPrependingRule).orElse(null) : null;
+  @Unique
+  private final Multimap<Item, Item> appendingRule = ExtShapeConfig.CURRENT_CONFIG.addToVanillaGroups ? Registries.ITEM_GROUP.getKey(this.group).map(VanillaItemGroup::getAppendingRule).orElse(null) : null;
 
   @Inject(method = "add", at = @At("HEAD"))
   public void preAdd(ItemStack stack, ItemGroup.StackVisibility visibility, CallbackInfo ci) {
-    if (ExtShapeConfig.CURRENT_CONFIG.addToVanillaGroups && allowTransitive) {
-      allowTransitive = false;
-      // 为了避免递归添加物品，故添加一个布尔值进行控制。下面的 postAdd 同理。
-      final Optional<RegistryKey<ItemGroup>> itemGroupKey = Registries.ITEM_GROUP.getKey(this.group);
-      if (itemGroupKey.isPresent()) {
-        for (Item item : VanillaItemGroup.getPrependingRule(itemGroupKey.get()).get(stack.getItem())) {
-          add(new ItemStack(item), visibility);
-        }
+    if (prependingRule != null) {
+      for (Item item : prependingRule.get(stack.getItem())) {
+        addSwiftly(new ItemStack(item), visibility);
       }
-      allowTransitive = true;
     }
   }
 
   @Inject(method = "add", at = @At("RETURN"))
   public void postAdd(ItemStack stack, ItemGroup.StackVisibility visibility, CallbackInfo ci) {
-    if (ExtShapeConfig.CURRENT_CONFIG.addToVanillaGroups && allowTransitive) {
-      allowTransitive = false;
-      // 为了避免递归添加物品，故添加一个布尔值进行控制。下面的 postAdd 同理。
-      final Optional<RegistryKey<ItemGroup>> itemGroupKey = Registries.ITEM_GROUP.getKey(this.group);
-      if (itemGroupKey.isPresent()) {
-        for (Item item : VanillaItemGroup.getAppendingRule(itemGroupKey.get()).get(stack.getItem())) {
-          add(new ItemStack(item), visibility);
-        }
+    if (appendingRule != null) {
+      for (Item item : appendingRule.get(stack.getItem())) {
+        addSwiftly(new ItemStack(item), visibility);
       }
-      allowTransitive = true;
     }
   }
 }
