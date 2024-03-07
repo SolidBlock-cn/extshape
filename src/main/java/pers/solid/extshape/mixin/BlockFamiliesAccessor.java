157678fc0c116b6b2f93d3830543ad25d08ac945
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2024-03-07 22:56:03 +0800

    更新至 2.1.4（合并的提交） 修复竖直台阶没有 uvlock 的问题 加入 conventional tags 并更新文档 将模组中增加的音频加入注册表；将彩色方块加入 fabric conventional tags 更新文档 完善纹理 微调了创造模式物品栏中部分物品的排序。一些已经在建筑方块中显示的物品不再在自然中重复显示 修复与铜有关的数据与原版命名空间重复的问题

--- src/main/java/pers/solid/extshape/mixin/BlockFamiliesAccessor.java
+++ src/main/java/pers/solid/extshape/mixin/BlockFamiliesAccessor.java
@@ -3,6 +3,7 @@ package pers.solid.extshape.mixin;
 import net.minecraft.block.Block;
 import net.minecraft.data.family.BlockFamilies;
 import net.minecraft.data.family.BlockFamily;
+import org.jetbrains.annotations.Contract;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
@@ -10,6 +11,7 @@ import java.util.Map;
 
 @Mixin(BlockFamilies.class)
 public interface BlockFamiliesAccessor {
+  @Contract
   @Accessor("BASE_BLOCKS_TO_FAMILIES")
   static Map<Block, BlockFamily> getBaseBlocksToFamilies() {
     throw new AssertionError("Cannot access: mixin is not loaded");
