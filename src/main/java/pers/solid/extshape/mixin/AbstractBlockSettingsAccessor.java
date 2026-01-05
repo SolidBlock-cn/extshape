0349c8af43eb864e2d615defa5df5edaeda434cb
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-01-05 10:49:45 +0800

    更新至 3.1.2，为发光方块添加 nonOpaque 以修复台阶不发光的问题

--- src/main/java/pers/solid/extshape/mixin/AbstractBlockSettingsAccessor.java
+++ src/main/java/pers/solid/extshape/mixin/AbstractBlockSettingsAccessor.java
@@ -1,11 +1,17 @@
 package pers.solid.extshape.mixin;
 
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.function.ToIntFunction;
 
 @Mixin(AbstractBlock.Settings.class)
 public interface AbstractBlockSettingsAccessor {
   @Accessor
   void setToolRequired(boolean toolRequired);
+
+  @Accessor
+  ToIntFunction<BlockState> getLuminance();
 }
