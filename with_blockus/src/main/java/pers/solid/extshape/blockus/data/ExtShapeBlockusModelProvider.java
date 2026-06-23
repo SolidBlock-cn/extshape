ed43060e31c7a6b9528d7c45b88615371e531822
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-06-23 14:05:44 +0800

    修复带釉陶瓦柱纹理缺失的问题 可能不需要优选至早期版本

--- with_blockus/src/main/java/pers/solid/extshape/blockus/data/ExtShapeBlockusModelProvider.java
+++ with_blockus/src/main/java/pers/solid/extshape/blockus/data/ExtShapeBlockusModelProvider.java
@@ -50,6 +50,7 @@ public class ExtShapeBlockusModelProvider extends ExtShapeModelProvider {
 
     for (var block : BlockusBlocks.GLAZED_TERRACOTTA_PILLAR.blocks().asList()) {
       registerSuffixed(block, TextureSlot.END, "_top");
+      registerSuffixed(block, TextureSlot.SIDE, "_side");
     }
 
     for (final Block block : BlockusBlocks.SMALL_LOGS.bundle().values()) {
